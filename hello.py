95b269700c9b883c1683655f7faf140ef3ad8b62
Bharath Kumar <buggabharath@gmail.com>
2024-10-05 22:40:53 +0530

    wrote sample oops examples

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
-print("hello world....")
+
+class Animal:
+    
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    
+    def __str__(self) -> str:
+        return f"Animal Name is {self.name}"
+    
+    
+def main():
+    dog = Animal("Bruno", 12)
+    print(dog)
+    
+
+if __name__ == "__main__":
+    main()
